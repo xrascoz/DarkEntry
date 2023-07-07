ffaf424c3292be4ebc2627eb8e4e92b248fb74c1
Rasco <xrascoz@gmail.com>
2023-07-07 17:41:46 +0300

    end the page serveis

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,26 @@ const cardWhyUs = document.querySelectorAll(".card-why-us");
 const cardWhyUsText = document.querySelectorAll(".card-why-us p");
 let activeElement = null;
 
-cardWhyUs.forEach((element, index) => {
-  cardWhyUsText[0].classList.add("active");
-  element.addEventListener("click", function () {
-    removeActiveLi();
-    element.classList.toggle("active");
-    let hight = cardWhyUsText[index].scrollHeight;
+if (cardWhyUs) {
+  cardWhyUs.forEach((element, index) => {
+    cardWhyUsText[0].classList.add("active");
+    element.addEventListener("click", function () {
+      removeActiveLi();
+      element.classList.toggle("active");
+      let hight = cardWhyUsText[index].scrollHeight;
 
-    if (activeElement !== element) {
-      cardWhyUsText[index].classList.add("active");
-      cardWhyUsText[index].style.height = `${hight}px`;
-      activeElement = element;
-    } else {
-      cardWhyUsText[index].style.height = `${0}px`;
-      cardWhyUsText[index].classList.remove("active");
-      activeElement = null;
-    }
+      if (activeElement !== element) {
+        cardWhyUsText[index].classList.add("active");
+        cardWhyUsText[index].style.height = `${hight}px`;
+        activeElement = element;
+      } else {
+        cardWhyUsText[index].style.height = `${0}px`;
+        cardWhyUsText[index].classList.remove("active");
+        activeElement = null;
+      }
+    });
   });
-});
+}
 
 function removeActiveLi() {
   cardWhyUs.forEach((element) => {
@@ -41,19 +43,22 @@ function removeActiveLi() {
 
 const cardLight = document.querySelectorAll('.card-light')
 
-const UPDATE = ({ x, y }) => {
-  cardLight.forEach(element => {
-    const BOUNDS = element.getBoundingClientRect()
-    const posX = x - BOUNDS.x
-    const posY = y - BOUNDS.y
-    const ratioX = posX / BOUNDS.width
-    const ratioY = posY / BOUNDS.height
-    element.style.setProperty('--ratio-x', ratioX)
-    element.style.setProperty('--ratio-y', ratioY)
-  });
+if (cardLight) {
+  const UPDATE = ({ x, y }) => {
+    cardLight.forEach(element => {
+      const BOUNDS = element.getBoundingClientRect()
+      const posX = x - BOUNDS.x
+      const posY = y - BOUNDS.y
+      const ratioX = posX / BOUNDS.width
+      const ratioY = posY / BOUNDS.height
+      element.style.setProperty('--ratio-x', ratioX)
+      element.style.setProperty('--ratio-y', ratioY)
+    });
+  }
+  document.body.addEventListener('pointermove', UPDATE)
 }
 
-document.body.addEventListener('pointermove', UPDATE)
+
 
 // <!-- ======================== Toggle NavPar Phone   ======================== --> 
 
@@ -61,15 +66,20 @@ document.body.addEventListener('pointermove', UPDATE)
 const toggle = document.querySelector('.toggle')
 const listWebsite = document.querySelector('.list-website')
 
-toggle.addEventListener("click", () => {
-  listWebsite.classList.toggle("active");
-});
 
-document.addEventListener("click", (event) => {
-  if (!listWebsite.contains(event.target) && event.target !== toggle) {
-    listWebsite.classList.remove("active");
-  }
-});
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    listWebsite.classList.toggle("active");
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!listWebsite.contains(event.target) && event.target !== toggle) {
+      listWebsite.classList.remove("active");
+    }
+  });
+}
+
+
 
 
 // <!-- ======================== Submit Search   ======================== -->
@@ -81,21 +91,25 @@ const submitSearch = document.querySelector(".submit-search");
 const xResults = document.querySelector(".x-results");
 const submitSearchCard = document.querySelector(".results-pop .card-div");
 
-submitSearch.addEventListener("click", (event) => {
-  event.preventDefault()
-  resultsPop.classList.add("active");
-})
+if (submitSearch) {
+  submitSearch.addEventListener("click", (event) => {
+    event.preventDefault()
+    resultsPop.classList.add("active");
+  })
 
-xResults.addEventListener("click", (event) => {
-  event.stopPropagation();
-  resultsPop.classList.remove("active");
-});
-
-document.addEventListener("click", (event) => {
-  if (resultsPop.classList.contains("active") && !submitSearchCard.contains(event.target) && !submitSearch.contains(event.target)) {
+  xResults.addEventListener("click", (event) => {
+    event.stopPropagation();
     resultsPop.classList.remove("active");
-  }
-});
+  });
+
+  document.addEventListener("click", (event) => {
+    if (resultsPop.classList.contains("active") && !submitSearchCard.contains(event.target) && !submitSearch.contains(event.target)) {
+      resultsPop.classList.remove("active");
+    }
+  });
+
+}
+
 
 
 // <!-- ======================== Toggle Popup Request Demo   ======================== --> 
@@ -105,31 +119,35 @@ const requestDemoPop = document.querySelector(".request-demo-pop");
 const xRequestDemo = document.querySelector(".x-request-demo");
 const RequestDemoCard = document.querySelector(".request-demo-pop .card-div");
 
-requestDemoButton.forEach(element => {
-  element.addEventListener("click", () => {
-    resultsPop.classList.remove("active");
-    requestDemoPop.classList.add("active");
+
+if (xRequestDemo) {
+  requestDemoButton.forEach(element => {
+    element.addEventListener("click", () => {
+      resultsPop.classList.remove("active");
+      requestDemoPop.classList.add("active");
+    });
   });
-});
 
-xRequestDemo.addEventListener("click", (event) => {
-  event.stopPropagation();
-  requestDemoPop.classList.remove("active");
-});
-
-document.addEventListener("click", (event) => {
-  if (requestDemoPop.classList.contains("active") && !RequestDemoCard.contains(event.target) && !isDescendant(requestDemoButton, event.target)) {
+  xRequestDemo.addEventListener("click", (event) => {
+    event.stopPropagation();
     requestDemoPop.classList.remove("active");
-  }
-});
+  });
 
-function isDescendant(parents, child) {
-  for (let i = 0; i < parents.length; i++) {
-    if (parents[i].contains(child)) {
-      return true;
+  document.addEventListener("click", (event) => {
+    if (requestDemoPop.classList.contains("active") && !RequestDemoCard.contains(event.target) && !isDescendant(requestDemoButton, event.target)) {
+      requestDemoPop.classList.remove("active");
     }
+  });
+
+  function isDescendant(parents, child) {
+    for (let i = 0; i < parents.length; i++) {
+      if (parents[i].contains(child)) {
+        return true;
+      }
+    }
+    return false;
   }
-  return false;
+
 }
 
 // <!-- ======================== Register Div Pop   ======================== -->
@@ -138,16 +156,19 @@ const registerDiv = document.querySelectorAll(".register-div .register")
 const registerPop = document.querySelector(".register-pop")
 const xRegisterPop = document.querySelector(".x-register-pop")
 
+if (registerDiv) {
+  registerDiv.forEach(element => {
+    element.addEventListener("click", () => {
+      registerPop.classList.add("active");
+    })
+  });
 
-registerDiv.forEach(element => {
-  element.addEventListener("click", () => {
-    registerPop.classList.add("active");
+  xRegisterPop.addEventListener("click", () => {
+    registerPop.classList.remove("active");
   })
-});
+}
 
-xRegisterPop.addEventListener("click", () => {
-  registerPop.classList.remove("active");
-})
+
 
 
 // <!-- ======================== Form Request Demo   ======================== -->
@@ -158,19 +179,23 @@ const cardDivChild = document.querySelector(".card-div-child")
 const emailSendDiv = document.querySelector(".email-send-div")
 const inputCardDivChild = document.querySelectorAll(".card-div-child input")
 
-formRequestDemo.addEventListener("submit", (e) => {
-  e.preventDefault()
 
-  cardDivChild.classList.add("disabled")
-  emailSendDiv.classList.remove("disabled")
+if (formRequestDemo) {
 
-  setTimeout(() => {
-    cardDivChild.classList.remove("disabled")
-    emailSendDiv.classList.add("disabled")
+  formRequestDemo.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-    inputCardDivChild.forEach(element => {
-      element.value = ""
-    });
-  }, 3000);
+    cardDivChild.classList.add("disabled")
+    emailSendDiv.classList.remove("disabled")
 
-})
+    setTimeout(() => {
+      cardDivChild.classList.remove("disabled")
+      emailSendDiv.classList.add("disabled")
+
+      inputCardDivChild.forEach(element => {
+        element.value = ""
+      });
+    }, 3000);
+
+  })
+}
